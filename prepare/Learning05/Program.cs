5ca6d59cc7166405caa0dfaa62845b3c8b66d46e
DMergy <mer21040@byui.edu>
2024-02-20 15:11:23 -0800

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Purple", 7);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Pink", 7, 14);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Magenta", 7);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            string sType = s.GetShapeType();
+
+            Console.WriteLine($"The {sType} is {color} and has an area of {area}.");
+        }
+
     }
+
 }
