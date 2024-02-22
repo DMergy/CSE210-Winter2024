810873854405a693c210c5af9c8394216c88981b
DMergy <mer21040@byui.edu>
2024-02-21 16:00:06 -0800

    GoalsAlmostThere

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -1,6 +1,139 @@
 using System;
+using System.IO;
+
+public class GoalManager
+
+{
+    private List<Goal> _goals = new List<Goal>();
+    private int _totalPoints;
+    public GoalManager()
+    {
+        _totalPoints = 0;
+    }
+    public void AddGoal(Goal goal)
+    {
+        _goals.Add(goal);
+    }
+    public int GetTotalPoints()
+    {
+        return _totalPoints;
+    }
+    public void AddPoints(int points)
+    {
+        _totalPoints += points;
+    }
+    public void AddBonus(int bonusPoints)
+    {
+        _totalPoints += bonusPoints;
+    }
+    public void SetTotalPoints(int totalPoints)
+    {
+        _totalPoints = totalPoints;
+    }
+    public List<Goal> GetGoalsList()
+    {
+        return _goals;
+    }
+
+    public void ListGoals()
+    {
+        if (_goals.Count() > 0)
+        {
+            Console.WriteLine("Your Goals are: ");
+
+            int index = 1;
+            foreach (Goal goal in _goals)
+            {
+                goal.ListGoal(index);
+                index = index + 1;
+            }
+        }
+        else
+        {
+            Console.WriteLine("You currently have no goals.");
+        }
+    }
+    public void RecordGoalEvent()
+    {
+        ListGoals();
+
+        Console.Write("Which goal did you accomplish? ");
+        int select = int.Parse(Console.ReadLine()) - 1;
+
+        int goalPoints = GetGoalsList()[select].GetPoints();
+        AddPoints(goalPoints);
+
+        GetGoalsList()[select].RecordGoalEvent(_goals);
+
+        Console.WriteLine($"*** You have {GetTotalPoints()} points! ***");
+    }
+    public void SaveGoals()
+    {
+        Console.Write("What is the name for this goal file? ");
+        string userInput = Console.ReadLine();
+        string userFileName = userInput + ".txt";
+
+        using (StreamWriter outputFile = new StreamWriter(userFileName))
+        {
+            outputFile.WriteLine(GetTotalPoints());
+            
+            foreach (Goal goal in _goals)
+            {
+                outputFile.WriteLine(goal.SaveGoal());
+            }
+        }
+    }
+
+    public void LoadGoals()
+    {
+        Console.Write("What is the name of your goal file? ");
+        string userInput = Console.ReadLine();
+        string userFileName = userInput + ".txt";
+
+        if (File.Exists(userFileName))
+        {
+            string[] readText = File.ReadAllLines(userFileName);
+
+            int totalPoints = int.Parse(readText[0]);
+            SetTotalPoints(totalPoints);
+            
+            readText = readText.Skip(1).ToArray();
+            
+            foreach (string line in readText)
+            {
+                string[] entries = line.Split("; ");
+
+                string type = entries[0];
+                string name = entries[1];
+                string description = entries[2];
+                int points = int.Parse(entries[3]);
+                bool status = Convert.ToBoolean(entries[4]);
+
+                if (entries[0] == "Simple Goal:")
+                {
+                    SimpleGoal sGoal = new SimpleGoal(type, name, description, points, status);
+                    AddGoal(sGoal);
+                }
+                if (entries[0] == "Eternal Goal:")
+                {
+                    EternalGoal eGoal = new EternalGoal(type, name, description, points, status);
+                    AddGoal(eGoal);
+                }
+                if (entries[0] == "Check List Goal:")
+                {
+                    int numberTimes = int.Parse(entries[5]);
+                    int bonusPoints = int.Parse(entries[6]);
+                    int counter = int.Parse(entries[7]);
+                    ChecklistGoal clGoal = new ChecklistGoal(type, name, description, points, status, numberTimes, bonusPoints, counter);
+                    AddGoal(clGoal);
+                }
+                
+            }
+        }
+    }
 
 
+}
 
 
 // _goals:List<Goal>
